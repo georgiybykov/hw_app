2c11f09c8e012203a829f8306296a67ad3af3c2c
Georgiy Bykov <zhora.bykov@gmail.com>
2021-11-24 04:01:45 +0300

    Add and configure dialyxir deps

--- test/hw_app_test.exs
+++ test/hw_app_test.exs
@@ -3,11 +3,11 @@ defmodule HWAppTest do
   doctest HWApp
 
   describe "#hello" do
-    test "when someone name has been passed" do
+    test "when the name of somebody has been passed" do
       assert HWApp.hello('Name') == "Hello Name!"
     end
 
-    test "when there is no name provided" do
+    test "when the name has not been provided" do
       assert HWApp.hello() == "Hello World!"
     end
   end
